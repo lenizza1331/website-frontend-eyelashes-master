6ed4fd077e774fed459e701cb90ac41d0846f378
lenizza1331 <leniyuva@gmail.com>
2022-04-18 04:03:38 +0300

    k

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-// import {useTransition, animated} from 'react-spring'
-// import NavigationMenu from './NavigationMenu'
 import React, { useState } from 'react'
 import {
     Link
